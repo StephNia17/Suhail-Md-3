89314bf84da38b06a15cde49a3e2c1b23cf5ec83
StephNia17 <alertsweepstakes@gmail.com>
2025-04-20 12:49:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ global.audio= "" ;
 global.video= "" ;
 global.port =process.env.PORT 
 global.appUrl=process.env.APP_URL || ""     // put your deploy app/bot url here, for 24/7 for (render , koyeb, glitch)
-global.email ="saimsamsun789@gmail.com"
-global.location="Lahore,Pakistan."
+global.email ="stephenallison505@gmail.com"
+global.location="Lagos,Africa."
 
 
 global.mongodb= process.env.MONGODB_URI || "false"  
@@ -18,21 +18,21 @@ global.DATABASE_URL = process.env.DATABASE_URL || "false"
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md-3";
-global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07";
-global.website =process.env.GURL || "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.ibb.co/TgwRyHR/Ephoto360-com-166de101e03cd7.jpg" || "https://i.ibb.co/bH1kbX0/a4c0b1af253197d4837ff6760d5b81c0.jpg" ; // SET LOGO FOR IMAGE 
-global.caption = process.env.CAPTION || global.caption || "Made With LOVE!" 
+global.gurl  =process.env.GURL  || "https://wa.me/2349051080824";
+global.website =process.env.GURL || "https://wa.me/2349051080824" ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.ibb.co/hYsRPvz/IMG-20241228-WA0116.jpg" || "https://i.ibb.co/4jGXCzw/IMG-20241228-WA0115.jpg" ; // SET LOGO FOR IMAGE 
+global.caption = process.env.CAPTION || global.caption || "𝑆𝑡𝑒𝑝ℎ𝑒𝑛 𝑁𝑖𝑎(𝐴𝑙𝑙𝑖𝑠𝑜𝑛) 2025 © | 𝐵𝑢𝑖𝑙𝑡 𝑤𝑖𝑡ℎ 𝑐𝑜𝑑𝑒, 𝑑𝑟𝑖𝑣𝑒𝑛 𝑏𝑦 𝑖𝑛𝑛𝑜𝑣𝑎𝑡𝑖𝑜𝑛" 
 
 
-global.devs = "" // Developer Contacts
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "";
+global.devs = "19803369857, 18632280160, 2349051080824, 2348024568393, 2349077716617, 2349042499358, 16624082457" // Developer Contacts
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "19803369857, 18632280160, 2349051080824, 2348024568393, 2349077716617, 2349042499358, 16624082457";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2349051080824";
 
 
 
 
 //========================= [ BOT SETTINGS ] ======8===================\\
-global.style = process.env.STYLE   || Math.floor(Math.random()*6) || '0'  // put '1' to "5" here to check bot styles
+global.style = process.env.STYLE   || Math.floor(Math.random()*6) || '4'  // put '1' to "5" here to check bot styles
 global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
 global.gdbye = process.env.GOODBYE || process.env.CAN_GOODBYE || "false"; 
 global.wlcm  = process.env.WELCOME || process.env.CAN_WELCOME || Math.floor(Math.random()*3)  === 1 ?  "true" : "false" ;  // Make it "false" for disable WELCOME 
@@ -42,21 +42,21 @@ global.disablepm = process.env.DISABLE_PM || "false"
 global.disablegroup = process.env.DISABLE_GROUPS || "false", // disable bot in groups when public mode
 
 global.MsgsInLog = process.env.MSGS_IN_LOG|| "false" // "true"  to see messages , "log" to show logs , "false" to hide logs messages
-global.userImages= process.env.USER_IMAGES || "" //"https://i.ibb.co/pXyNHj8/suhail.jpg" // "image" // set Image/video urls here
-global.waPresence= process.env.WAPRESENCE ||  "null" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
+global.userImages= process.env.USER_IMAGES || "https://i.ibb.co/C6nzgr3/IMG-20241225-WA0101-1.jpg" //"https://i.ibb.co/QYfC9Bk/IMG-20241225-WA0110.jpg" // "image" // set Image/video urls here
+global.waPresence= process.env.WAPRESENCE ||  "recording" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
 
 
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
 global.readcmds = process.env.READ_COMMAND || "false"
 global.readmessage = process.env.READ_MESSAGE || "false"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "19803369857, 18632280160, 2349051080824, 2348024568393, 2349077716617, 2349042499358, 16624082457";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
-global.read_status = process.env.AUTO_READ_STATUS || "false"
+global.read_status = process.env.AUTO_READ_STATUS || "true"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923184474176,923xxxxxxxx";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "19803369857, 18632280160, 2349051080824, 2348024568393, 2349077716617, 2349042499358, 16624082457";
 
 global.api_smd = "https://api-smd.onrender.com/" //  || "https://api-smd-1.vercel.app" // expires
 global.scan = "https://suhail-md-vtsf.onrender.com";
@@ -66,17 +66,17 @@ global.SESSION_ID = process.env.SESSION_ID  || ""  // PUT your SESSION_ID
 
 module.exports = {
 
-  menu: process.env.MENU || "", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
+  menu: process.env.MENU || "2", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
 
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "3.0.1-developement",
-  caption : global.caption || "" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ GlobalTechInfo ® 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@GlobalTechInfo"),
+  caption : global.caption || "𝑆𝑡𝑒𝑝ℎ𝑒𝑛 𝑁𝑖𝑎(𝐴𝑙𝑙𝑖𝑠𝑜𝑛) 2025 © | 𝐵𝑢𝑖𝑙𝑡 𝑤𝑖𝑡ℎ 𝑐𝑜𝑑𝑒, 𝑑𝑟𝑖𝑣𝑒𝑛 𝑏𝑦 𝑖𝑛𝑛𝑜𝑣𝑎𝑡𝑖𝑜𝑛" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ GlobalTechInfo ® 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@GlobalTechInfo"),
  
-  author : process.env.PACK_AUTHER|| "",
-  packname: process.env.PACK_NAME || "",
-  botname : process.env.BOT_NAME  || "Suhail-MD",
-  ownername:process.env.OWNER_NAME|| "Suhail:Shy<3",
+  author : process.env.PACK_AUTHER|| "𝐴𝑙𝑙𝑖𝑠𝑜𝑛",
+  packname: process.env.PACK_NAME || "𝐻 𝐼 𝑁 𝐴 𝐾 𝐴 𝐼",
+  botname : process.env.BOT_NAME  || "𝐻 𝐼 𝑁 𝐴 𝐾 𝐴 𝐼",
+  ownername:process.env.OWNER_NAME|| "𝑆𝑡𝑒𝑝ℎ𝑒𝑛 𝑁𝑖𝑐ℎ𝑜𝑙𝑎𝑠 𝐴𝑙𝑙𝑖𝑠𝑜𝑛",
 
 
   errorChat : process.env.ERROR_CHAT || "",
@@ -91,7 +91,7 @@ module.exports = {
 
 
   WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
-  LANG: ( process.env.THEME ||  "SUHAIL"  ).toUpperCase(),
+  LANG: ( process.env.THEME ||  "GIDEON"  ).toUpperCase(),
 
 
 
